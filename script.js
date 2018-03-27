559aa29ff10fca464a02c584e802c6a8daf9aff0
Damon <dpullan13@gmail.com>
2018-03-27 09:49:32 -0700

    small changes to js

--- script.js
+++ script.js
@@ -15,23 +15,20 @@ function decrypt(encrypted, key) {
     return decrypted;
 }
 function encryptThis() {
-    myPlaintext = document.getElementById('mPlaintext').value;
-    myKey = document.getElementById('mKey').value;
+    getPlainKey();
     let encrypted = encrypt(myPlaintext, myKey);
     document.getElementById("encryptedText").innerHTML = encrypted;
 }
 function encryptAll() {
-    myPlaintext = document.getElementById('mPlaintext').value;
-    myKey = document.getElementById('mKey').value;
+    getPlainKey();
     $.getJSON("http://danycabrera.com/csc130/proxy.php?key=1wqnh5q6cthLz1lyHTF6sqQhYB7rca6RUGJy57y2EOgY&sheet=Sheet1", processData);
 }
 function AlertTest() {
-    myPlaintext = document.getElementById('mPlaintext').value;
-    myKey = document.getElementById('mKey').value;
+    getPlainKey();
     alert(myPlaintext);
 }
 function processData(myData) {
-    myPlaintext = document.getElementById('mPlaintext').value;
+    getPlainKey();
     string += "<tr><th>Key</th><th>Encrypted Message</th><th>Decrypted Message</th><th>Original Message</th></tr>";
     for (i in myData) {
         let encrypted = encrypt(myPlaintext, myData[i].key);
