552800c978395ad5ed2594ef35d9eed014adf317
Damon <dpullan13@gmail.com>
2018-03-27 09:38:50 -0700

    Fixed issue with user inputs not appearing

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var myPlaintext = document.getElementById('myPlaintext').innerHTML.value;
-var myKey = document.getElementById("myKey").innerHTML.value;
+var myPlaintext = "";
+var myKey = "";
 let keys = [];
 let string = "";
+function getPlainKey() {
+    myPlaintext = document.getElementById('mPlaintext').value;
+    myKey = document.getElementById('mKey').value;
+}
 function encrypt(plaintext, key) {
     let encrypted = CryptoJS.AES.encrypt(plaintext, key);
     return encrypted;
@@ -11,16 +15,23 @@ function decrypt(encrypted, key) {
     return decrypted;
 }
 function encryptThis() {
+    myPlaintext = document.getElementById('mPlaintext').value;
+    myKey = document.getElementById('mKey').value;
     let encrypted = encrypt(myPlaintext, myKey);
     document.getElementById("encryptedText").innerHTML = encrypted;
 }
 function encryptAll() {
-    document.getElementById("table1").innerHTML = string;
+    myPlaintext = document.getElementById('mPlaintext').value;
+    myKey = document.getElementById('mKey').value;
+    $.getJSON("http://danycabrera.com/csc130/proxy.php?key=1wqnh5q6cthLz1lyHTF6sqQhYB7rca6RUGJy57y2EOgY&sheet=Sheet1", processData);
 }
 function AlertTest() {
+    myPlaintext = document.getElementById('mPlaintext').value;
+    myKey = document.getElementById('mKey').value;
     alert(myPlaintext);
 }
 function processData(myData) {
+    myPlaintext = document.getElementById('mPlaintext').value;
     string += "<tr><th>Key</th><th>Encrypted Message</th><th>Decrypted Message</th><th>Original Message</th></tr>";
     for (i in myData) {
         let encrypted = encrypt(myPlaintext, myData[i].key);
@@ -33,9 +44,7 @@ function processData(myData) {
         string += "<td>" + myPlaintext + "</td>";
         string += "</tr>";
     }
+    document.getElementById("table1").innerHTML = string;
 }
-$(document).ready(function() {
-    $.getJSON("http://danycabrera.com/csc130/proxy.php?key=1wqnh5q6cthLz1lyHTF6sqQhYB7rca6RUGJy57y2EOgY&sheet=Sheet1", processData);
-  });
 
   
